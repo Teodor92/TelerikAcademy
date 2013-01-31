445f4e31b20db1b347d50b9f355029acab68e50c
TeodorKurtev <teodor.ivanov92@gmail.com>
2013-01-31 12:28:48 +0200

    Done the "Bracket checker" with Stack

---
+++ CSharpHomeworks/12. StringsAndTextProcessing/03.1. BracketCheckerWithStack/BracketCheckerWithStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BracketCheckerWithStack
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        Stack<string> brackets = new Stack<string>();
+        bool areCorrect = true;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                brackets.Push("(");
+            }
+            else if (input[i] == ')')
+            {
+                if (brackets.Count == 0)
+                {
+                    areCorrect = false;
+                    break;
+                }
+                else
+                {
+                    brackets.Pop();
+                }
+            }
+        }
+
+        if (brackets.Count != 0)
+        {
+            areCorrect = false;
+        }
+
+        Console.WriteLine("The brackets are correctly put: {0}", areCorrect);
+    }
+}
