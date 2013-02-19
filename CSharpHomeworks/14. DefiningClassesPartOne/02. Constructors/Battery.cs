e7cd4e3552022de070d70c3508c30ea8522b6001
TeodorKurtev <teodor.ivanov92@gmail.com>
2013-02-19 14:55:02 +0200

    Home14 - task 2

---
+++ CSharpHomeworks/14. DefiningClassesPartOne/02. Constructors/Battery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+public class Battery
+{
+    private string batteryModel;
+    private int hoursIdle;
+    private int hoursTalked;
+
+    public Battery(string baterryModel) : this(baterryModel, 0, 0)
+    { 
+    
+    }
+
+    public Battery(string baterryModel, int hoursIdle) : this(baterryModel, hoursIdle, 0)
+    { 
+    
+    }
+
+    public Battery(string baterryModel, int hoursIdle, int hoursTalked)
+    {
+        this.batteryModel = baterryModel;
+        this.hoursIdle = hoursIdle;
+        this.hoursTalked = hoursTalked;
+    }
+
+
+    public string BatteryModel
+    {
+        get { return this.batteryModel; }
+        set { this.batteryModel = value; }
+    }
+
+    public int HoursIdle
+    {
+        get { return this.hoursIdle; }
+        set { this.hoursIdle = value; }
+    }
+
+    public int HoursTalked
+    {
+        get { return this.hoursTalked; }
+        set { this.hoursTalked = value; }
+    }
+}
