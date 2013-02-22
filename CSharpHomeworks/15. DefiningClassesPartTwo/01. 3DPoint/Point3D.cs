dd32ee6217b1da56b3f05fb0c53b0a9f8af8d5f6
TeodorKurtev <teodor.ivanov92@gmail.com>
2013-02-22 11:48:27 +0200

    Moved struct to own file

---
+++ CSharpHomeworks/15. DefiningClassesPartTwo/01. 3DPoint/Point3D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+public struct Point3D
+{
+    public int pointX { get; set; }
+    public int pointY { get; set; }
+    public int pointZ { get; set; }
+
+    static private Point3D zero = new Point3D(0, 0, 0);
+
+    public Point3D(int pointX, int pointY, int pointZ)
+        : this()
+    {
+        this.pointX = pointX;
+        this.pointY = pointY;
+        this.pointZ = pointZ;
+    }
+
+    public static Point3D Zero
+    {
+        get
+        {
+            return zero;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder endText = new StringBuilder();
+        endText.AppendFormat("Point X: {0}", this.pointX.ToString());
+        endText.AppendLine();
+        endText.AppendFormat("Point Y: {0}", this.pointY.ToString());
+        endText.AppendLine();
+        endText.AppendFormat("Point Z: {0} \n-", this.pointZ.ToString());
+        return endText.ToString();
+    }
+}
