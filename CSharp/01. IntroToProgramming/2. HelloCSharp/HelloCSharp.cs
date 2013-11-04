3573e915425a7052679620b677f6367691929e71
Teodor92 <theodor_kyrtev@abv.bg>
2013-11-04 20:49:43 +0200

    File structures change

---
+++ CSharp/01. IntroToProgramming/2. HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿// 2. Create, compile and run a “Hello C#” console application.
+
+using System;
+
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+
